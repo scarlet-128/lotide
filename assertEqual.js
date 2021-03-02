44b17e2f4214377ba6816c13391d5d2d2fd1a9f2
Scarlet Shao <scarletshao@gmail.com>
2021-03-02 06:30:07 +0000

    add assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`); 
+  } else {
+    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`) }
+    
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
