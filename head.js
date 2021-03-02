347650672ebb2cfcbc276294ba77a8cf47fc1bb8
Scarlet Shao <scarletshao@gmail.com>
2021-03-02 06:29:24 +0000

    add head

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`); 
+  } else {
+    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`) }
+    
+};
+
+function head (arr){
+  return arr[0];
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 'undefined');
