43165dd833b9442adf2a4cce2825c7ba4158719a
Scarlet Shao <scarletshao@gmail.com>
2021-03-04 23:50:34 +0000

    with eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,22 @@
-const eqArrays = function (x,y) {
+const eqArrays = function(x,y) {
   let output;
-  if (x.length = y.length) {
-  for (let i =0; i < x.length; i++){
-    if (x[i] === y[i]){
-      output = true;
-    } else {
-      output = false;
+  if (x.length === y.length) {
+    for (let i = 0; i < x.length; i++) {
+      if (x[i] === y[i]) {
+        output = true;
+      } else {
+        output = false;
+      }
     }
-  }
-} return output;
-}
-
-const assertArraysEqual = function (actual, expected){
-  if (eqArrays(actual,expected)){
+  } return output;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
     console.log("passed");
   } else {
     console.log("failed");
   }
-}
+};
 
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
