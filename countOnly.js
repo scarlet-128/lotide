99d672b6796d8c06c2b8e7b68bddf1c6d8e49fe2
Scarlet Shao <scarletshao@gmail.com>
2021-03-04 01:32:28 +0000

    add countOnly

---
+++ countOnly.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+};
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
