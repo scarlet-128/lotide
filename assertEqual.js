a557b760c9778f070f8a108fab9101ed6738cd31
Scarlet Shao <scarletshao@gmail.com>
2021-03-04 06:49:16 +0000

    new assertEqual with eslint style

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`); 
+    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`) }
-    
+    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`);
+  }    
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
