13b689551a8e16d201d5c3ac3d2e26249a1c6e50
Scarlet Shao <scarletshao@gmail.com>
2021-03-08 20:45:27 +0000

    add the test file to existing one

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🍚🍚🍚Assertion Failed: ${actual} !== ${expected}`);
   }    
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😙😃😃Assertion Passed: ${actual} === ${expected}`); 
-  } else {
-    console.log(`😞😞😞Assertion Failed: ${actual} !== ${expected}`) }
-    
-};
-
+const assertEqual = require('./assertEqual');
 function head (arr){
   return arr[0];
 }
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 'undefined');
+module.exports = head;
